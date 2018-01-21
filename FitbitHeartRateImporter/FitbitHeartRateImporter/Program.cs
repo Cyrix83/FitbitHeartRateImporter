60a1d143aa0ad54da16e2777f019e00742a66181
robert jarlvik <cyrix83@hotmail.com>
2018-01-21 10:53:29 +0100

    Initial commit

---
+++ FitbitHeartRateImporter/FitbitHeartRateImporter/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace FitbitHeartRateImporter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
